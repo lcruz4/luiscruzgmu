2005ed8ca832f75a19db446410263e95a5cc175b
luis <luiscruzgmu@gmail.com>
2023-08-19 21:14:08 -0400

    Projects under construction

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,12 +1,15 @@
 import styled from '@emotion/styled';
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import useNavIntersection from '../../hooks/useNavIntersection';
 import { NavItems } from '../../types';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 
-const StylishSwiper = styled(Swiper)(({ theme }) => `
+const StylishSwiper = styled(Swiper)(
+  ({ theme }) => `
   height: 500px;
   width: 100%;
 
@@ -28,28 +31,50 @@ const StylishSwiper = styled(Swiper)(({ theme }) => `
     height: 100%;
     object-fit: cover;
   }
-`);
+`,
+);
 
 export const Projects = () => {
-  const ref = useNavIntersection(.5, NavItems.Projects);
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  const navRef = useNavIntersection(0.5, NavItems.Projects);
+
+  useEffect(() => {
+    if (inView) {
+      document.getElementsByTagName('video')[0].play();
+    } else {
+      document.getElementsByTagName('video')[0].pause();
+    }
+  }, [inView]);
 
   return (
-    <div ref={ref}>
-      <StylishSwiper className='z-0'
+    <div ref={navRef}>
+      <StylishSwiper
+        className='z-0'
         pagination={{
           dynamicBullets: true,
         }}
         modules={[Pagination]}
       >
-        <SwiperSlide>Project 1</SwiperSlide>
-        <SwiperSlide>Project 2</SwiperSlide>
-        <SwiperSlide>Project 3</SwiperSlide>
-        <SwiperSlide>Project 4</SwiperSlide>
-        <SwiperSlide>Project 5</SwiperSlide>
-        <SwiperSlide>Project 6</SwiperSlide>
-        <SwiperSlide>Project 7</SwiperSlide>
-        <SwiperSlide>Project 8</SwiperSlide>
-        <SwiperSlide>Project 9</SwiperSlide>
+        <SwiperSlide>⛔️ Under Construction ⛔️</SwiperSlide>
+        <SwiperSlide>Nothing here</SwiperSlide>
+        <SwiperSlide>Seriously there's nothing here</SwiperSlide>
+        <SwiperSlide>You don't need to check every slide</SwiperSlide>
+        <SwiperSlide>...</SwiperSlide>
+        <SwiperSlide>...</SwiperSlide>
+        <SwiperSlide>There's nothing here</SwiperSlide>
+        <SwiperSlide>Come on man!</SwiperSlide>
+        <SwiperSlide>
+          <video
+            ref={ref}
+            controls
+            className='w-full h-full bg-black'
+            tabIndex={-1}
+          >
+            <source src='/videos/neverGonnaGiveYouUp.mp4' type='video/mp4' />
+          </video>
+        </SwiperSlide>
       </StylishSwiper>
     </div>
   );
