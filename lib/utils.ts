84270a169e4addcf5bae8eb014d258c9cb03ed99
luis <luis.cruz@ingenovishealth.com>
2026-04-12 15:25:26 -0400

    kittysort and added verify macro droid

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { NextApiRequest, NextApiResponse } from 'next';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const verifyMacroDroid = (req: NextApiRequest, res: NextApiResponse) => {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader || !authHeader.startsWith('Basic ')) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return false;
+  }
+
+  const base64 = authHeader.slice(6); // strip "Basic "
+  const decoded = Buffer.from(base64, 'base64').toString('utf-8');
+  const [username, password] = decoded.split(':');
+
+  if (username !== process.env.MACRODROID_USERNAME || password !== process.env.MACRODROID_PASSWORD) {
+    res.status(403).json({ error: 'Forbidden' });
+    return false;
+  }
+  return true;
+};
